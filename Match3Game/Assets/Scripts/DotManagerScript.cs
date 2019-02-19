9572498722c6e552d2e5af10f3449bad36476bf3
Gabriel P <gaberofl42@gmail.com>
2019-02-19 16:07:46 +1100

    Progress on Match3 Mechanic
    DotScript now adds the selected gameobject to a list
    DotManagerScript now goes through all gameobjects in the list

---
+++ Match3Game/Assets/Scripts/DotManagerScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+public class DotManagerScript : MonoBehaviour
+{
+
+    public List<GameObject> Peices;
+    public int ColourPoint;
+    public int NumOfPeices;
+    public bool CheckConnection;
+    private void Start()
+    {
+        CheckConnection = false;
+    }
+
+    private void Update()
+    {
+        if (CheckConnection)
+        {
+            for (NumOfPeices = 0; NumOfPeices < Peices.Capacity; NumOfPeices++)
+            {
+                if (Peices[NumOfPeices].tag == "Green")
+                {
+                    Debug.Log("Green");
+                    ColourPoint += 1;
+                }
+                else
+                {
+                    CheckConnection = false;
+                    Peices.Clear();
+                    break;
+                }
+            }
+         
+        }
+         
+    }
+
+ }
