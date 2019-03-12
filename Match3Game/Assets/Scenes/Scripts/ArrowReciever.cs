0094c1c90afb1349c308535125ba8d19bd9109c3
AIE\Max Turner <max.leslie.turner@gmail.com>
2019-03-12 15:35:33 +1100

    Started the creature selection

---
+++ Match3Game/Assets/Scenes/Scripts/ArrowReciever.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowReciever : MonoBehaviour {
+
+    public GameObject gameManager;
+    private CreatureSelect creatureSelectScript;
+
+	// Use this for initialization
+	void Awake () {
+        creatureSelectScript = gameManager.GetComponent<CreatureSelect>();
+	}
+
+
+    public void RightButtonClicked()
+    {
+        creatureSelectScript.RightArrowClicked();
+    }
+
+    public void LeftButtonClicked()
+    {
+        creatureSelectScript.LeftArrowClicked();
+    }
+}
---
+++ Match3Game/Assets/Scenes/Scripts/CreatureSelect.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatureSelect : MonoBehaviour {
+
+    public GameObject gobu;
+    public GameObject koko;
+    public GameObject binky;
+
+    public bool middleCreature = true;
+    public bool leftCreature = false;
+    public bool rightCreature = false;
+
+
+    void Awake()
+    {
+        gobu.SetActive(true);
+        koko.SetActive(false);
+        binky.SetActive(false);
+        rightCreature = false;
+        middleCreature = true;
+        leftCreature = false;
+    }
+    public void RightArrowClicked()
+    {
+        if(middleCreature == true)
+        {
+            gobu.SetActive(false);
+            koko.SetActive(true);
+            rightCreature = true;
+            middleCreature = false;
+        }
+        if(leftCreature == true)
+        {
+            gobu.SetActive(true);
+            binky.SetActive(false);
+            leftCreature = false;
+            middleCreature = true;
+        }
+        if(rightCreature == true)
+        {
+            koko.SetActive(false);
+            binky.SetActive(true);
+            rightCreature = false;
+            leftCreature = true;
+        }
+    }
+    public void LeftArrowClicked()
+    {
+        if(middleCreature == true)
+        {
+            gobu.SetActive(false);
+            binky.SetActive(true);
+            leftCreature = true;
+            middleCreature = false;
+        }
+        if(leftCreature == true)
+        {
+            binky.SetActive(false);
+            koko.SetActive(true);
+            leftCreature = false;
+            rightCreature = true;
+        }
+        if(rightCreature == true)
+        {
+            koko.SetActive(false);
+            gobu.SetActive(true);
+            rightCreature = false;
+            middleCreature = true;
+        }
+    }
+
+
+ 
+}
