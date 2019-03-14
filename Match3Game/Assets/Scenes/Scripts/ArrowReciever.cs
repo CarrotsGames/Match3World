9b9b40b8f7eef0e9bf2956ed869eb4fb75560649
AIE\Max Turner <max.leslie.turner@gmail.com>
2019-03-14 14:11:37 +1100

    Added the trail to all levels

--- Match3Game/Assets/Scenes/Scripts/ArrowReciever.cs
+++ Match3Game/Assets/Scenes/Scripts/ArrowReciever.cs
@@ -25,7 +25,7 @@ public class ArrowReciever : MonoBehaviour {
 
     public void CreatureClicked()
     {
-        creatureSelectScript.LoadCreatureLevels();
+        creatureSelectScript.StartTransitions();
     }
 
 }
