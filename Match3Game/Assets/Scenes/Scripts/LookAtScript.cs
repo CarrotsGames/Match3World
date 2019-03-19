bf9a713a18ed20ddf1353f70d941e16cbd66367c
Gabriel P <gaberofl42@gmail.com>
2019-03-20 10:09:22 +1100

    EyeMovement
    Companion eyes follow mouse

---
+++ Match3Game/Assets/Scenes/Scripts/LookAtScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtScript : MonoBehaviour
+{
+
+    public float Speed;
+    public float Limit;
+    private Vector3 center;
+
+    private void Start()
+    {
+        // referneces Starting position 
+        center = transform.position;
+    }
+    private void Update()
+    {
+        // Gets position of mouse
+        Vector3 Pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Pos.z = 0.0f;
+        //moves eyes position towards position of mouse 
+        Vector3 Direction = (Pos - transform.position) * Speed;
+        // Clamps Position of eyes by limit
+        Direction = Vector3.ClampMagnitude(Direction, Limit);
+        // Moves eyes 
+        Vector3 Eyeroll = center + Direction;
+        transform.position = center + Direction;
+    }
+
+}
