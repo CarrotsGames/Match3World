00bebf6c785cff775c04400c7738b2837de328f4
Gabriel P <gaberofl42@gmail.com>
2019-03-25 12:02:31 +1100

    Backbutton support
    Backbutton on android phones now return you back to menu and quit game

--- Match3Game/Assets/Scenes/Scripts/MobileExit/MobileQuitGame.cs
+++ Match3Game/Assets/Scenes/Scripts/MobileExit/MobileQuitGame.cs
@@ -1,36 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class MobileExitScript : MonoBehaviour {
 
-    private GameObject GameTransitionsGameObj;
-    private GameObject RealTimerGameObj;
+public class MobileQuitGame : MonoBehaviour {
+
+     private GameObject RealTimerGameObj;
     private RealTimeCounter RealTimeScript;
-    GameTransitions GT;
+ 
     // Update is called once per frame
     // Exits the game using Android Back Button 
 
     private void Start()
-    {
-        GameTransitionsGameObj = GameObject.FindGameObjectWithTag("GameTransition");
-        GT = GameTransitionsGameObj.GetComponent<GameTransitions>();
+    { 
         RealTimerGameObj = GameObject.FindGameObjectWithTag("MainCamera");
         RealTimeScript = RealTimerGameObj.GetComponent<RealTimeCounter>();
 
     }
 
-    void Update ()
+    void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            GT.HomeButton();
+            Application.Quit();
         }
-         
-         
+
+
     }
     private void OnApplicationQuit()
     {
         RealTimeScript.ResetClock();
     }
-
 }
