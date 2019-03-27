a8adfbc46229ec82b6bedc76ea8be29849da6597
Gabriel P <gaberofl42@gmail.com>
2019-03-27 11:33:54 +1100

    DailySpin
    Added DailySpin wheel to MainScene

--- Match3Game/Assets/Scenes/Scripts/TimeScripts/EventScript.cs
+++ Match3Game/Assets/Scenes/Scripts/TimeScripts/EventScript.cs
@@ -3,37 +3,53 @@ using System.Collections;
 
 public class EventScript : MonoBehaviour
 {
+    public GameObject DailySpinner;
+    // sets how long to count down Timers
+    public float TimeToDoDaily;
+
+    // Counts down timers in script 
     public float DailyTimer;
-    public float WeeklyTimer;
-    public float MonthlyTimer;
-    public bool CannotDoDaily;
+    private float WeeklyTimer;
+    private float MonthlyTimer;
+    public bool CanDoDaily;
     // Use this for initialization
     private GameObject RealTimerGameObj;
     private RealTimeCounter RealTimeScript;
-
+    private DailySpin dailySpinScript;
     private void Start()
     {
         DailyTimer = PlayerPrefs.GetFloat("DAILYTEST1");
         RealTimerGameObj = GameObject.FindGameObjectWithTag("MainCamera");
         RealTimeScript = RealTimerGameObj.GetComponent<RealTimeCounter>();
+        DailySpinner.SetActive(false);
+        dailySpinScript = DailySpinner.GetComponent<DailySpin>();
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
+    
     public void DoDailyButton()
     {
+        DailyTimer = RealTimeScript.DailyTimerCountDown;
         if (RealTimeScript.DailyTimerCountDown < 0)
         {
             //SPIN WHEEL
             // RESET TIMER
-            RealTimeScript.DailyTimerCountDown = 300;
+            DailyTimer = TimeToDoDaily;
+            RealTimeScript.DailyTimerCountDown = DailyTimer;
             PlayerPrefs.SetFloat("DAILYTEST1", DailyTimer);
-            CannotDoDaily = true;
+
+            RealTimeScript.DailyTimerCountDown = DailyTimer;
+            CanDoDaily = true;
+        }
+        if(CanDoDaily)
+        {
+             DailySpinner.SetActive(true);
+         }
+        else
+        {
+            dailySpinScript.TimeToStopWheelStore = dailySpinScript.TimeToStopWheel;
+            dailySpinScript.StartUpSpeedStore = dailySpinScript.StartUpSpeed;
+            dailySpinScript.MaxSpeedStore = dailySpinScript.MaxSpeed;
         }
      }
 
