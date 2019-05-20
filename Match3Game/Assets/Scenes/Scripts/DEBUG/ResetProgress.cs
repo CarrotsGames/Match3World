7a7f2b2bc2e738145f7fc924b0fecdb4b24a992f
Gabriel P <gaberofl42@gmail.com>
2019-05-20 12:15:34 +1000

    Reset
    ResetButton now resets analytics

--- Match3Game/Assets/Scenes/Scripts/DEBUG/ResetProgress.cs
+++ Match3Game/Assets/Scenes/Scripts/DEBUG/ResetProgress.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using PlayFab;
+using PlayFab.ClientModels;
 public class ResetProgress : MonoBehaviour {
 
     public bool Reset;
     public GameObject NodeManager;
     public GameObject Unlockables;
-	// Use this for initialization
-	void Start () {
+    public GameObject PowerUpManGameObj;
+
+    // Use this for initialization
+    void Start () {
         Reset = false;
 
     }
@@ -17,14 +20,48 @@ public class ResetProgress : MonoBehaviour {
          if (Reset)
         {
             Debug.Log("RESET");
+         
+            // resets score
             NodeManager.GetComponent<DotManager>().TotalScore = 0;
-            // Unlockables.GetComponent<UnlockableCreatures>().UnlockableMoobling[0] = "";
+       
+            // Resets analytics 
             PlayerPrefs.SetString("BINKY", "Nothing");
-            //  Unlockables.GetComponent<UnlockableCreatures>().UnlockableMoobling[1] = "";
             PlayerPrefs.SetString("KOKO", "Nothing");
             PlayerPrefs.SetString("CRIUS", "Nothing");
-
             PlayerPrefs.SetString("UNLOCKED", "Nothing");
+            PlayerPrefs.SetFloat("GOBUSCORETRACK", 0);
+            PlayerPrefs.SetFloat("BINKYSCORETRACK", 0);
+            PlayerPrefs.SetFloat("KOKOSCORETRACK", 0);
+            PlayerPrefs.SetFloat("CRIUSSCORETRACK", 0);
+           
+            // resets powerups
+            PowerUpManGameObj.GetComponent<PowerUpManager>().NumOfShuffles = 5;
+            PowerUpManGameObj.GetComponent<PowerUpManager>().NumOfBombs = 5;
+            PowerUpManGameObj.GetComponent<PowerUpManager>().NumOfSCR = 5;
+            PowerUpManGameObj.GetComponent<PowerUpManager>().NumOfMultilpiers = 5;
+
+            PlayFabClientAPI.UpdateUserData(new UpdateUserDataRequest()
+            {
+                Data = new Dictionary<string, string>()
+            {
+                { "Gobu"   ,"SCORE: "+  0 },
+                { "Binky"   ,"SCORE: "+  0 },
+                { "Koko"   ,"SCORE: "+  0 },
+                { "Crius"   ,"SCORE: "+  0 },
+                { "BinkyTIME"   ,"" + 0},
+                { "CriusTIME"   ,"" + 0},
+                { "GobuTIME"   ,"" + 0},
+                { "KokoTIME"   , "" + 0 }
+            },
+         
+            },
+      result => Debug.Log("Analytics Sent"),
+      error =>
+      {
+          Debug.Log("Got error setting user data Ancestory to Jacob");
+          Debug.Log(error.GenerateErrorReport());
+
+      });
         }
     }
     // Update is called once per frame
