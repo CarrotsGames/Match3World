475cbe510533a7745c139e76554fd6e67ea2df41
Gabriel P <gaberofl42@gmail.com>
2019-05-20 11:41:25 +1000

    Debug
    Added debug button

--- Match3Game/Assets/Scenes/Scripts/DEBUG/ResetProgress.cs
+++ Match3Game/Assets/Scenes/Scripts/DEBUG/ResetProgress.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class ResetProgress : MonoBehaviour {
 
+    public bool Reset;
+    public GameObject NodeManager;
+    public GameObject Unlockables;
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        Reset = false;
+
+    }
+    private void Update()
+    {
+         if (Reset)
+        {
+            Debug.Log("RESET");
+            NodeManager.GetComponent<DotManager>().TotalScore = 0;
+            // Unlockables.GetComponent<UnlockableCreatures>().UnlockableMoobling[0] = "";
+            PlayerPrefs.SetString("BINKY", "Nothing");
+            //  Unlockables.GetComponent<UnlockableCreatures>().UnlockableMoobling[1] = "";
+            PlayerPrefs.SetString("KOKO", "Nothing");
+            PlayerPrefs.SetString("UNLOCKED", "Nothing");
+        }
+    }
+    // Update is called once per frame
+    public void ResetData ()
+    {
+        Reset = true;
+    }
 }
