c72a251ba95d27f0d6d75f01bbe25cb5c77a9da9
Gabriel P <gaberofl42@gmail.com>
2019-05-21 14:29:58 +1000

    Sounds
    Added Pop sound when a successful chain is done
    Added FireWork sound to Firework ParticleEffect

--- Match3Game/Assets/Scenes/Scripts/ParticleDecay.cs
+++ Match3Game/Assets/Scenes/Scripts/ParticleDecay.cs
@@ -1,23 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+// DESTORYS PARTICLE EFFECTS AFTER NODES ARE DESTROYED TO CLEAR MEMORY
 public class ParticleDecay : MonoBehaviour
 {
     float DecayTimer;
-
+    private AudioSource Source;
+    public AudioClip ParticleAudio;
     // Use this for initialization
     void Start()
     {
+        Source = GetComponent<AudioSource>();
         DecayTimer = 1.5f;
+        Source.clip = ParticleAudio;
+        Source.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
+         
         DecayTimer -= Time.deltaTime;
         if(DecayTimer < 0)
         {
             Destroy(gameObject);
+         
         }
     }
 }
