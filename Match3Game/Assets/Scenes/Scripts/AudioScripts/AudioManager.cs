14eec9e7649cdfd82755bb8ab46395ec2369e127
Gabriel P <gaberofl42@gmail.com>
2019-05-28 15:21:31 +1000

    SoundManager
    Added soundmanager script to control all sound made in the game

---
+++ Match3Game/Assets/Scenes/Scripts/AudioScripts/AudioManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    public AudioClip[] NodeAudio;
+    public AudioSource NodeSource;
+    public AudioClip[] MooblingAudio;
+    public AudioSource MooblingSource;
+    public AudioClip[] ParticleAudio;
+    public AudioSource ParticleSource;
+ 
+    public bool soundOn;
+    private string SaveBool;
+    // Use this for initialization
+    private void Start()
+    {
+        soundOn = true;
+        soundOn = (PlayerPrefs.GetInt(SaveBool) != 0);
+    }
+    // Update is called once per frame
+    void Update ()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            soundOn = false;
+            // saves bool as false
+            PlayerPrefs.SetInt(SaveBool, (soundOn ? 1 : 0));
+
+            Debug.Log("OFF");
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            soundOn = true;
+            Debug.Log("ON");
+            // saves bool as true
+            PlayerPrefs.SetInt(SaveBool, (soundOn ? 1 : 0));
+
+        }
+        if (!soundOn)
+        {
+            //Disable audioSource
+            NodeSource.enabled = false;
+            MooblingSource.enabled = false;
+            ParticleSource.enabled = false;
+        }
+        else
+        {
+            NodeSource.enabled = true;
+            MooblingSource.enabled = true;
+            ParticleSource.enabled = true;
+
+            //enable audio source
+        }
+    }
+}
