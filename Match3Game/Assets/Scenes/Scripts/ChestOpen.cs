d3daf2b7a1f8f9449888dd7187a8ccd86d414706
AIE\Max Turner <max.leslie.turner@gmail.com>
2019-06-17 15:32:59 +1000

    squishy anim

---
+++ Match3Game/Assets/Scenes/Scripts/ChestOpen.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestOpen : MonoBehaviour {
+
+    public GameObject chestWalls;
+    public GameObject openChest;
+    public Animator anim;
+
+    public int waitTimer = 5;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(OpenChest());
+        openChest.SetActive(false);
+	}
+
+
+    IEnumerator OpenChest ()
+    {
+        yield return new WaitForSeconds(waitTimer);
+        anim.SetBool("StartAnim", true);
+        
+    }
+
+
+    public void AnimDone()
+    {
+        Destroy(chestWalls);
+        openChest.SetActive(true);
+        Destroy(this.gameObject);
+    }
+
+}
