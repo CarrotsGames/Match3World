033c2ebe8982fc46e36fbd408cf33ee67f6050be
AIE\Max Turner <max.leslie.turner@gmail.com>
2019-06-19 16:33:52 +1000

    fixed splash screen

--- Match3Game/Assets/Scenes/Scripts/CloseMenu.cs
+++ Match3Game/Assets/Scenes/Scripts/CloseMenu.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 public class CloseMenu : MonoBehaviour {
 
     public GameObject unlockScreen;
+    public GameObject egg;
+    public GameObject superCR;
+    public GameObject shuffle;
+    public GameObject bomb;
+    public GameObject multi;
+
 
     public void TurnOffTab()
     {
         unlockScreen.SetActive(false);
+        egg.SetActive(false);
+        superCR.SetActive(false);
+        shuffle.SetActive(false);
+        bomb.SetActive(false);
+        multi.SetActive(false);
     }
 
 }
