3b59bf2de1bdaee75196eea59cbc665672007e14
Gabriel P <gaberofl42@gmail.com>
2019-06-24 11:47:10 +1000

    Added Shop values
    Added values to the new currency shop
    Linked values to the google dev cloud

---
+++ Match3Game/Assets/PurchaseMoney.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PurchaseMoney : MonoBehaviour {
+
+    public PowerUpManager PowerUpManagerGameObj;
+   
+    public void GrantGold(int Gold)
+    {
+        PowerUpManagerGameObj.GetComponent<PowerUpManager>().Currency += Gold;
+        Debug.Log("You have recieved" + Gold + "Gold!!!");
+    }
+    public void GrantBundle(int Gold)
+    {
+        PowerUpManagerGameObj.GetComponent<PowerUpManager>().Currency += Gold;
+        // Bombs
+        PowerUpManagerGameObj.GetComponent<PowerUpManager>().NumOfBombs += 1;
+        //Super Colour Remover
+        PowerUpManagerGameObj.GetComponent<PowerUpManager>().NumOfSCR += 1;
+        //Super Shuffle
+        PowerUpManagerGameObj.GetComponent<PowerUpManager>().NumOfShuffles += 1;
+        //Super Multlpier
+        PowerUpManagerGameObj.GetComponent<PowerUpManager>().NumOfMultilpiers += 1;
+        Debug.Log("You have recieved" + Gold + "Gold!!! " +
+            "\n You have receiced PowerUps ");
+
+    }
+    public void GrantSuperBundle(int Gold)
+    {
+        PowerUpManagerGameObj.GetComponent<PowerUpManager>().Currency += Gold;
+        // Bombs
+        PowerUpManagerGameObj.GetComponent<PowerUpManager>().NumOfBombs += 10;
+        //Super Colour Remover
+        PowerUpManagerGameObj.GetComponent<PowerUpManager>().NumOfSCR += 10;
+        //Super Shuffle
+        PowerUpManagerGameObj.GetComponent<PowerUpManager>().NumOfShuffles += 10;
+        //Super Multlpier
+        PowerUpManagerGameObj.GetComponent<PowerUpManager>().NumOfMultilpiers += 10;
+        Debug.Log("You have recieved" + Gold + "Gold!!! " +
+            "\n You have receiced PowerUps ");
+
+    }
+}
