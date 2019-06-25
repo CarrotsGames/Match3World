1f6f4f3d5a4739b832a59be5720a3967c04096de
AIE\Max Turner <max.leslie.turner@gmail.com>
2019-06-25 12:14:44 +1000

    okami in and ready for gbe

---
+++ Match3Game/Assets/PlayCatPurr.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayCatPurr : MonoBehaviour
+{
+
+    public AudioSource sceneaudio;
+    public AudioClip purr;
+
+
+
+    public void PlaySound()
+    {
+        sceneaudio.PlayOneShot(purr);
+    }
+}
