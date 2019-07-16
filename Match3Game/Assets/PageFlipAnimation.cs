21d3fcf9598e5e5ddd7753e11918e62a65422659
AIE\Max Turner <max.leslie.turner@gmail.com>
2019-07-16 16:41:22 +1000

    added page flip animation and added some sounds

--- Match3Game/Assets/PageFlipAnimation.cs
+++ Match3Game/Assets/PageFlipAnimation.cs
@@ -6,11 +6,17 @@ public class PageFlipAnimation : MonoBehaviour
 {
     public GameObject paperSprite;
 
-
+    public AudioClip pageFlip;
+    public AudioSource audioSource;
 
 
     public void StopAnim()
     {
         paperSprite.SetActive(false);
+    }
+
+    public void PlayAudio()
+    {
+        audioSource.PlayOneShot(pageFlip, 0.7f);
     }
 }
---
+++ Match3Game/Assets/StartGame.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    public Animator fadeAnim;
+    public Animator gameAnim;
+
+    public GameObject startText;
+
+    public AudioClip pageFlip;
+    public AudioSource audioSource;
+
+
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Main Screen");
+    }
+
+    public void StartFade()
+    {
+        fadeAnim.SetBool("Start", true);
+    }
+
+    public void StartAnim()
+    {
+        gameAnim.SetBool("AnimStart",true);
+        startText.SetActive(false);
+    }
+
+
+    public void PlayAudio()
+    {
+        audioSource.PlayOneShot(pageFlip, 0.7f);
+    }
+
+
+}
