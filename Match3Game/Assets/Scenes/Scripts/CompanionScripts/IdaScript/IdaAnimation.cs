e0f08ca74610d098cae153ffdaf83afd3f8789dc
AIE\Max Turner <max.leslie.turner@gmail.com>
2019-07-18 14:35:38 +1000

    idasaurous animation set up

--- Match3Game/Assets/Scenes/Scripts/CompanionScripts/IdaScript/IdaAnimation.cs
+++ Match3Game/Assets/Scenes/Scripts/CompanionScripts/IdaScript/IdaAnimation.cs
@@ -8,6 +8,8 @@ public class IdaAnimation : MonoBehaviour
     private GameObject DestroyNodeGameObj;
     private DestroyNodes DestoryNodeScript;
 
+    public Animator idaAnimator;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,14 +21,23 @@ public class IdaAnimation : MonoBehaviour
     void Update()
     {
         if(DestoryNodeScript.NormalCombo)
-        {
+        { 
             Debug.Log("NormalComboAnim");
-
+            idaAnimator.SetBool("Normal Combo", true);
         }
         else if (DestoryNodeScript.BigCombo)
         {
             Debug.Log("BigComboAnim");
+            idaAnimator.SetBool("Big Combo", true);
 
         }
+    }
+
+
+    public void ResetAll()
+    {
+        idaAnimator.SetBool("Big Combo", false);
+        idaAnimator.SetBool("Normal Combo", false);
+
     }
 }
