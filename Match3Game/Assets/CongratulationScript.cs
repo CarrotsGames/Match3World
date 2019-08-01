5cf541f0fc1f1cfb570181d3c477f1e65d997308
AIE\Max Turner <max.leslie.turner@gmail.com>
2019-08-01 17:03:54 +1000

    congraths ui

---
+++ Match3Game/Assets/CongratulationScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class CongratulationScript : MonoBehaviour
+{
+
+    public GameObject congratsCanvus;
+    public GameObject CriusUnlockImage;
+    public GameObject OkamiUnlockImage;
+    public GameObject SaucoUnlockImage;
+    public GameObject ChickPeaUnlockImage;
+    public GameObject SquishyUnlockImage;
+    public GameObject MoneyUnlockImage;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (congratsCanvus.active && Input.GetMouseButtonDown(0))
+        {
+            congratsCanvus.SetActive(false);
+            CriusUnlockImage.SetActive(false);
+            OkamiUnlockImage.SetActive(false);
+            SaucoUnlockImage.SetActive(false);
+            SquishyUnlockImage.SetActive(false);
+            ChickPeaUnlockImage.SetActive(false);
+            MoneyUnlockImage.SetActive(false);
+        }
+    }
+}
