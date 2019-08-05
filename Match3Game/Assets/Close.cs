ab30566730c5d9a83b97f0b164201503a3174292
AIE\Max Turner <max.leslie.turner@gmail.com>
2019-08-06 09:15:43 +1000

    koko and binkie unlock pop ups should now work

---
+++ Match3Game/Assets/Close.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Close : MonoBehaviour
+{
+    public GameObject menu;
+
+
+
+    public void CloseMenu()
+    {
+        menu.SetActive(false);
+    }
+}
