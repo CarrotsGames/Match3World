4b291549cee231e0674706358d8afee18541f9de
AIE\Max Turner <max.leslie.turner@gmail.com>
2019-08-15 16:19:00 +1000

    added new power up canvus

---
+++ Match3Game/Assets/PowerUpSlider.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpSlider : MonoBehaviour
+{
+    public Animator powerUpAnim;
+
+    public bool open;
+
+
+    public void Start()
+    {
+        open = false;
+    }
+
+
+    public void ButtonAnim()
+    {
+        if(open == false)
+        {
+            powerUpAnim.SetBool("Open", true);
+            powerUpAnim.SetBool("Close", false);
+            open = true;
+        }
+        else
+        {
+            powerUpAnim.SetBool("Close", true);
+            powerUpAnim.SetBool("Open", false);
+            open = false;
+        }
+    }
+
+
+
+
+
+
+
+}
