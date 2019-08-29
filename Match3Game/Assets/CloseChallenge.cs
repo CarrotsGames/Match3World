37d2683540740ec68f6f16e3c9bc796e22ca2f36
AIE\Max Turner <max.leslie.turner@gmail.com>
2019-08-29 15:28:03 +1000

    added panels for background for challenge and zen

---
+++ Match3Game/Assets/CloseChallenge.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseChallenge : MonoBehaviour
+{
+
+    public GameObject[] challengeFaces;
+    public GameObject challengeCanvus;
+
+
+    public void PanelReset()
+    {
+        challengeCanvus.SetActive(false);
+        for (int i = 0; i < challengeFaces.Length; i++)
+        {
+            challengeFaces[i].SetActive(false);
+        }
+    }
+
+
+
+
+}
