b730f19b26782000f7dbe11861c2b94b46a6e531
Gabriel P <gaberofl42@gmail.com>
2019-09-04 12:27:50 +1000

    Next challenge
    Next challenge button now loads next challenge

---
+++ Match3Game/Assets/Scenes/Scripts/Challenge/NextChallenge.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class NextChallenge : MonoBehaviour
+{
+    public string ChallengeScene;
+    public void NextChallengeButton()
+    {
+         
+        Scene CurrentScene = SceneManager.GetActiveScene();
+        ChallengeScene = CurrentScene.name;
+        int Index = PlayerPrefs.GetInt("ChallengeIndex");
+        Index += 1;
+        PlayerPrefs.SetInt("ChallengeIndex", Index);
+        SceneManager.LoadScene(ChallengeScene);
+
+    }
+}
