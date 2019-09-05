bf16732925a6190586c6833e3b5e5c8a42670842
Gabriel P <gaberofl42@gmail.com>
2019-09-05 14:07:55 +1000

    Challenge scenes
    You can now leave and retry challenge levels

---
+++ Match3Game/Assets/Scenes/Scripts/Challenge/RetryChallenge.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RetryChallenge : MonoBehaviour
+{
+    public string ChallengeScene;
+
+    public void Retry()
+    {
+        Scene CurrentScene = SceneManager.GetActiveScene();
+        ChallengeScene = CurrentScene.name;
+        SceneManager.LoadScene(ChallengeScene);
+    }
+}
