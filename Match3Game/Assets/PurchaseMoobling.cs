9c954e80d7addc5134e585f491f99f3ba42420fb
Gabriel P <gaberofl42@gmail.com>
2019-10-17 16:51:08 +1100

    Added moobling purchases

---
+++ Match3Game/Assets/PurchaseMoobling.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PurchaseMoobling : MonoBehaviour
+{
+    private GameObject Unlockables;
+    private void Start()
+    {
+        Unlockables = GameObject.Find("UNLOCKABLESMain");
+    }
+   public void BuyMoobling (string Moobling)
+    {
+        Unlockables = GameObject.Find("UNLOCKABLESMain");
+
+        Unlockables.GetComponent<UnlockableCreatures>().CompanionName = Moobling;
+        Unlockables.GetComponent<UnlockableCreatures>().Unlock();
+    }
+}
