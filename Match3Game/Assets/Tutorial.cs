b774bcb1866526b8d248b5879bd13c04dbaceccb
AIE\Max Turner <max.leslie.turner@gmail.com>
2019-10-22 10:30:40 +1100

    finished tutorial

--- Match3Game/Assets/Tutorial.cs
+++ Match3Game/Assets/Tutorial.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class Tutorial : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject[] tutParts;
+    public GameObject tutorialCanvus;
+
+
+    public void LoadPart2()
     {
-        
+        tutParts[0].SetActive(false);
+        tutParts[1].SetActive(true);
+    }
+    public void LoadPart3()
+    {
+        tutParts[1].SetActive(false);
+        tutParts[2].SetActive(true);
+    }
+    public void LoadPart4()
+    {
+        tutParts[2].SetActive(false);
+        tutParts[3].SetActive(true);
+    }
+    public void LoadPart5()
+    {
+        tutParts[3].SetActive(false);
+        tutParts[4].SetActive(true);
+    }
+    public void LoadPart6()
+    {
+        tutParts[4].SetActive(false);
+        tutParts[5].SetActive(true);
+    }
+    public void LoadPart7()
+    {
+        tutParts[5].SetActive(false);
+        tutParts[6].SetActive(true);
+    }
+    public void LoadPart8()
+    {
+        tutParts[6].SetActive(false);
+        tutParts[7].SetActive(true);
+    }
+    public void Close()
+    {
+        tutorialCanvus.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
+
 }
