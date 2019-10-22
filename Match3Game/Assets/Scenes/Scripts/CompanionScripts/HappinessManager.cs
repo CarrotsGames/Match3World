03739bbab10910e3423ebc956976e1f2c9112c42
Gabriel P <gaberofl42@gmail.com>
2019-10-22 16:56:05 +1100

    Added gold rewards to all scenes

--- Match3Game/Assets/Scenes/Scripts/CompanionScripts/HappinessManager.cs
+++ Match3Game/Assets/Scenes/Scripts/CompanionScripts/HappinessManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
@@ -35,34 +36,37 @@ public class HappinessManager : MonoBehaviour
     public Text NextLevel;
     public Text CurrentMultiplier;
     public Text CurrentHappiness;
+    public Text GoldRewardText;
     public Image FillColour;
     public Slider HappinessSlider;    
     public Animator Anim;
    
-    // Plays ad when sleeping
+    // Plays ads
     private GameObject SleepAd;
     private GameObject RealTimeGameObj;
+    private GameObject PowerUpManagerGameObj;
     private PlayLevelAd PlayLevelAdScript; 
     private BoardScript BoardScriptRef;
-
+    private PowerUpManager PowerUpManagerScript;
+    private List<int> GoldRewardList;
 
     // Use this for initialization
     void Start()
     {
-        //checks if players in main scene
+       
+
         // if they are then the happinessStates void wont be called to avoid mixing saves
         Scene CurrentScene = SceneManager.GetActiveScene();
         // Gets current scene
         SceneName = CurrentScene.name;
       
         SleepAd = GameObject.FindGameObjectWithTag("SleepingAd");
-        // If for somereason level is 0 make it 1
-        // this is used as a safety net just incase for somereason its 0
-    
+         // this is used as a safety net just incase for somereason its 0
         if(Level > 1)
         {
             CanGetCurrency = true;
         }
+        //checks if players in main scene
         if (SceneName == "Main Screen" || SceneName == "Gobu Tutorial")
         {
             OnMainScene = true;
@@ -72,7 +76,6 @@ public class HappinessManager : MonoBehaviour
             PlayLevelAdScript = SleepAd.GetComponent<PlayLevelAd>();
         }
         
-   
         // Gets companions current level
         CompanionSave = Companion.name + "Value";
           
@@ -81,35 +84,21 @@ public class HappinessManager : MonoBehaviour
         if (SceneName != "Gobu Tutorial")
         {
             BoardScriptRef = Board.GetComponent<BoardScript>();
-        }   // clamps hunger of selected companion from 0 to 100
-        // Slowly counts down Happiness value
-        //HappinessSliderValue -= Time.deltaTime / 6;
-        if (GameObject.Find("CHALLENGE") == null )
-        {
-            SaveSystem.LoadMoobling();
-            Level = SaveSystem.LoadMoobling().Level;
-            HappinessSliderValue = SaveSystem.LoadMoobling().EXP;
-            HappinessClamp += Level * 250;
-            HappinessClamp = SaveSystem.LoadMoobling().TotalEXP;
-           // DotManager.TotalScore = SaveSystem.LoadMoobling().TotalScore;
-            HappinessBar();     
-            LevelUpCanvas.SetActive(false);
         }
-        HappinessSlider.maxValue = HappinessClamp;
-        HappinessSliderValue = Mathf.Clamp(HappinessSliderValue, 0, HappinessClamp);
-        HappinessSlider.value = HappinessSliderValue;
-        //HappinessSlider.value = HappinessSliderValue;
+        LoadSaves();
+     
+     
         // if level is 0 score will not go up because nodes * level value equal score
         if (Level == 0)
         {
             Level = 1;
         }
+
         int NextLevelNum = Level + 1;
         NextLevel.text = " " + NextLevelNum;
-        //HappinessClamp = 100;
         LevelText.text = "" + Level;
         CurrentMultiplier.text = "" + Level;
-     
+    
         if(CompanionSave == "TutorialGobuValue")
         {
             Level = 2;
@@ -119,8 +108,42 @@ public class HappinessManager : MonoBehaviour
 
         }
     }
-
-  
+    void LoadSaves()
+    {
+        if (GameObject.Find("CHALLENGE") == null)
+        {         
+            SaveSystem.LoadMoobling();
+            Level = SaveSystem.LoadMoobling().Level;
+            HappinessSliderValue = SaveSystem.LoadMoobling().EXP;
+            HappinessClamp += Level * 250;
+            HappinessClamp = SaveSystem.LoadMoobling().TotalEXP;
+            // DotManager.TotalScore = SaveSystem.LoadMoobling().TotalScore;
+            HappinessBar();
+            LevelUpCanvas.SetActive(false);
+            PowerUpManagerGameObj = GameObject.FindGameObjectWithTag("PUM");
+            PowerUpManagerScript = PowerUpManagerGameObj.GetComponent<PowerUpManager>();
+        }
+        HappinessSlider.maxValue = HappinessClamp;
+        HappinessSliderValue = Mathf.Clamp(HappinessSliderValue, 0, HappinessClamp);
+        HappinessSlider.value = HappinessSliderValue;
+        AssignGold();
+    }
+    void AssignGold()
+    {
+        GoldRewardList = new List<int>() ;
+        for (int i = 0; i < 25; i++)
+        {
+            
+            if (i <= 20)
+            {
+                GoldRewardList.Add(5); 
+            }
+            else
+            {
+                GoldRewardList.Add(20);
+            }
+        }
+    }
     public void HappinessBar()
     {
              
@@ -151,20 +174,18 @@ public class HappinessManager : MonoBehaviour
         {
             if (HappinessSliderValue > HappinessClamp)
             {
-
                 LevelUpCanvas.SetActive(true);
                 FillColour.color = Color.green;
                 // Animation 
  
                 //  PlayerPrefs.SetInt(SaveStrings, (IsSleeping ? 1 : 0));
-               // Level = PlayerPrefs.GetInt(Companion.name + "Multiplier", Level);
+                // Level = PlayerPrefs.GetInt(Companion.name + "Multiplier", Level);
                 Level++;
                 PlayerPrefs.SetInt(Companion.name + "Multiplier", Level);
                 CanGetCurrency = true;
                 if (Level == 5 || Level == 10 || Level == 15 || Level == 20 || Level == 25)
                 {
                     PlayLevelAdScript.PlayAdNow();
-
                 }
                 // Music Change
                 // Add multiplier    
@@ -179,7 +200,10 @@ public class HappinessManager : MonoBehaviour
                 CurrentMultiplier.text = "" + Level;
                 // sets current exp
                 CurrentHappiness.text = "Current EXP:" + HappinessSliderValue + "/" + HappinessClamp;
-
+                PowerUpManagerScript.Currency += GoldRewardList[Level];
+                PowerUpManagerScript.PowerUpSaves();
+                 
+                GoldRewardText.text = "" + GoldRewardList[Level];
             }
         }
         else
