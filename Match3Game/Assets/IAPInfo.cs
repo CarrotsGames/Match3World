628fe44d0fdf205954230dc08b983388c29f3eb8
AIE\Max Turner <max.leslie.turner@gmail.com>
2019-10-31 14:40:35 +1100

    updated Money Scene
    info for what each IAP includes

---
+++ Match3Game/Assets/IAPInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IAPInfo : MonoBehaviour
+{
+
+    public GameObject info;
+    private bool infoUp;
+
+    public void LoadInfo()
+    {
+        if (infoUp == false)
+        {
+            info.SetActive(true);
+            infoUp = true;
+        }else
+        {
+            info.SetActive(false);
+            infoUp = false;
+        }
+    }
+}
