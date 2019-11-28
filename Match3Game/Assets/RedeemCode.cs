a87ce3aab6d8b1fd3a35cbad614a20eba98cc700
Gabriel P <gaberofl42@gmail.com>
2019-11-28 11:15:15 +1100

    Added redeem code

---
+++ Match3Game/Assets/RedeemCode.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class RedeemCode : MonoBehaviour
+{
+    public int Gold;
+    public string SpecialCode;
+    private GameObject PowerUpManagerGameObj;
+    private bool RedeemedCode = false;
+    public InputField Code;
+
+
+    public void RedemptionCode()
+    {
+        RedeemedCode = (PlayerPrefs.GetInt("CodeRedeemed1") != 0);
+        
+        if (Code.text == SpecialCode && !RedeemedCode)
+        {
+            PowerUpManagerGameObj = GameObject.FindGameObjectWithTag("PUM");
+            PowerUpManagerGameObj.GetComponent<PowerUpManager>().Currency += Gold;
+            PowerUpManagerGameObj.GetComponent<PowerUpManager>().PowerUpSaves();
+            RedeemedCode = true;
+            PlayerPrefs.SetInt("CodeRedeemed1", (RedeemedCode ? 1 : 0));
+        }  
+        else if (Code.text != SpecialCode)
+        {
+            Debug.Log("Invalid code");
+
+        }
+        else if (RedeemedCode)
+        {
+            Debug.Log("code Redeemed");
+        }
+    }
+
+}
